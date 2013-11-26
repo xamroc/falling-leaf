de19379cd9dd84b59c66e351f5ed3e4377102cff
Marco Lau <lau.career@gmail.com>
2013-11-26 22:22:33 +0800

    Hack a smoother falling leaf animation using jQuery

--- js/falling-leaf.js
+++ js/falling-leaf.js
@@ -1,3 +1,45 @@
-$(function() {
-	$(".test").draggable();
+$(document).ready(function() {
+	$("#leaf").animate({
+		top: "200px",
+		left: "400px"
+	}, {
+		duration: 1000,
+		specialEasing: {top: "easeOutQuad", left: "easeInQuad"},
+		complete: function() {
+			$("#leaf").animate({
+				top: "250px",
+				left: "450px"	
+			}, {
+				duration: 200,
+				specialEasing: {top: "easeInQuad", left: "easeOutQuad"},
+				complete: function() {
+					$("#leaf").animate({
+						top: "400px",
+						left: "50px"	
+					}, {
+						duration: 1000,
+						specialEasing: {top: "easeOutQuad", left: "easeInQuad"},
+						complete: function() {
+							$("#leaf").animate({
+								top: "450px",
+								left: "0px"	
+							}, {
+							duration: 200,
+							specialEasing: {top: "easeInQuad", left: "easeOutQuad"},
+							complete: function() {
+								$("#leaf").animate({
+									top: "600px",
+									left: "400px"	
+								}, {
+									duration: 1000,
+									specialEasing: {top: "easeOutQuad", left: "easeInQuad"}
+									});
+								}
+							});
+						}
+					});
+				}
+			});
+		}
+	});
 });
