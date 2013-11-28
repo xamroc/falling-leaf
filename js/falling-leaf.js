1727627b8499c3d4c2a379382b9bfc234177c5f8
Marco Lau <lau.career@gmail.com>
2013-11-28 15:58:03 +0800

    Write a natural looking falling leaf movement

--- js/falling-leaf.js
+++ js/falling-leaf.js
@@ -1,38 +1,51 @@
+/* A leaf animation that descends slowly and tries to mimic natural falling motion.
+
+*/
+
 $(document).ready(function() {
 	$("#leaf").animate({
-		top: "200px",
-		left: "400px"
+		top: "+=150",
+		left: "+=100"
 	}, {
-		duration: 1000,
+		duration: 600,
 		specialEasing: {top: "easeOutQuad", left: "easeInQuad"},
 		complete: function() {
 			$("#leaf").animate({
-				top: "250px",
-				left: "450px"	
+				top: "+=50px",
+				left: "+=50px"	
 			}, {
-				duration: 200,
+				duration: 400,
 				specialEasing: {top: "easeInQuad", left: "easeOutQuad"},
 				complete: function() {
 					$("#leaf").animate({
-						top: "400px",
-						left: "50px"	
+						top: "+=150px",
+						left: "-=400px"	
 					}, {
-						duration: 1000,
+						duration: 1800,
 						specialEasing: {top: "easeOutQuad", left: "easeInQuad"},
 						complete: function() {
 							$("#leaf").animate({
-								top: "450px",
-								left: "0px"	
+								top: "+=50px",
+								left: "-=50px"	
 							}, {
-							duration: 200,
-							specialEasing: {top: "easeInQuad", left: "easeOutQuad"},
-							complete: function() {
-								$("#leaf").animate({
-									top: "600px",
-									left: "400px"	
-								}, {
-									duration: 1000,
-									specialEasing: {top: "easeOutQuad", left: "easeInQuad"}
+								duration: 600,
+								specialEasing: {top: "easeInQuad", left: "easeOutQuad"},
+								complete: function() {
+									$("#leaf").animate({
+										top: "+=100px",
+										left: "+=150px"	
+									}, {
+										duration: 1000,
+										specialEasing: {top: "easeOutQuad", left: "easeInQuad"},
+										complete: function() {
+											$("#leaf").animate({
+												top: "+=50px",
+												left: "+=50px"	
+											}, {
+												duration: 1000,
+												specialEasing: {top: "easeInQuad", left: "easeOutQuad"}	
+											});
+										}
 									});
 								}
 							});
